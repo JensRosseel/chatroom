d193a89c59a6205c5b3bed6cae39cb91968a28e9
JensRosseel <slaythehorizon.games@gmail.com>
2021-09-06 10:25:03 +0200

    Add chatengine

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import React from 'react';
+import { ChatEngine } from 'react-chat-engine';
+
 function App() {
   return (
-   null
+    <ChatEngine
+      projectID='c6868c85-147a-468c-9b22-bfe21187626b'
+      userName='JohnDoe'
+      userSecret='P@ssw0rd'
+    />
   );
 }
 
