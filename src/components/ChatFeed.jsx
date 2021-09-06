b3a181612cf5911f1eb22b8d94f2c7fc8753de25
kdrbek06 <kdrbek@gmail.com>
2021-09-06 11:43:50 +0200

    Create ChatFeed's components

---
+++ src/components/ChatFeed.jsx
@@ -0,0 +1,16 @@
+import MessageForm from "./MessageForm";
+import MyMessage from "./MyMessage";
+import TheirMessage from "./TheirMessage";
+
+
+
+const ChatFeed = () => {
+    return (
+        <div>
+            ChatFeed
+        </div>
+    );
+
+}
+
+export default ChatFeed;
